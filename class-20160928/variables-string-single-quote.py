fe1cdb32dae3c2903b355b018b44c7c282bbb062
Chinseok Lee <allieuslee@gmail.com>
2016-09-28 09:24:34 +0900

    문자열 샘플코드 추가

---
+++ class-20160928/variables-string-single-quote.py
@@ -0,0 +1,31 @@
+
+name1 = 'Chinseok'
+
+print('\n---- lyrics1 ----')
+lyrics1 = 'The snow glows white on the mountain tonight'
+'Now a footprint to be seen'
+'A kingdom of isolation'
+'and it looks like I\'m the queen'
+print(lyrics1)
+
+print('\n---- lyrics2 ----')
+lyrics2 = ('The snow glows white on the mountain tonight\n' +
+        'Now a footprint to be seen\n' +
+        'A kingdom of isolation\n' +
+        'and it looks like I\'m the queen')
+print(lyrics2)
+
+print('\n---- lyrics3 ----')
+lyrics3 = ('The snow glows white on the mountain tonight\n'
+          'Now a footprint to be seen\n'
+          'A kingdom of isolation\n'
+          'and it looks like I\'m the queen')
+print(lyrics3)
+
+print('\n---- lyrics4 ----')
+lyrics4 = '''The snow glows white on the mountain tonight
+Now a footprint to be seen
+A kingdom of isolation
+and it looks like I'm the queen'''
+print(lyrics4)
+
